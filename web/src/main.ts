3105045d8d65b2170386ce8074108f692cc5d46e
João Robson Reis Jr <jreisam@outlook.com>
2025-06-02 01:48:18 -0400

    chore: minor ui adjustments

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,3 @@
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { enableProdMode, ApplicationRef } from '@angular/core';
@@ -6,25 +5,20 @@ import { enableDebugTools } from '@angular/platform-browser';
 import environment from './environments/environment';
 import 'zone.js';
 
-// Ativar modo de produção se não estiver em desenvolvimento
 if (environment.production) {
   enableProdMode();
 }
 
-// Configurações mais modernas para inicialização
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
-    // Preservar espaço em branco
     preserveWhitespaces: false,
-    // Usar JIT em vez de AOT (opcional, depende do seu fluxo de compilação)
     ngZoneEventCoalescing: true,
-    ngZoneRunCoalescing: true
+    ngZoneRunCoalescing: true,
   })
   .then(moduleRef => {
     // Ferramenta de depuração para Angular DevTools
     const applicationRef = moduleRef.injector.get(ApplicationRef);
     const componentRef = applicationRef.components[0];
-    // Isso habilita o suporte para o Angular DevTools
     if (!environment.production) {
       enableDebugTools(componentRef);
     }
